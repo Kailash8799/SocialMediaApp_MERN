a09d05e62c618a17e80a84d1293e5f0620c35610
kailash8799 <kailashrajput8799@gmail.com>
2023-07-27 11:22:10 +0530

    added story component

--- src/components/subcomponents/OneStory.jsx
+++ src/components/subcomponents/OneStory.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const OneStory = () => {
   return (
-    <div className="h-48 mx-2 rounded-lg shadow-inner border-b-[1px] border-slate-300 dark:border-slate-800 storywidth cursor-pointer shadow-slate-400 dark:shadow-slate-600 ">
+    <div className="h-48 mx-2 rounded-lg shadow-inner border-b-[1px] border-slate-300 dark:border-slate-800 storywidth cursor-pointer shadow-slate-400 dark:shadow-slate-600 flex-shrink-0">
       OneStory
     </div>
   );
--- src/components/subcomponents/Stories.jsx
+++ src/components/subcomponents/Stories.jsx
@@ -3,9 +3,21 @@ import OneStory from "./OneStory";
 
 const Stories = () => {
   return (
-    <div className="items-center w-full h-56 mx-auto sm:mx-0 sm:max-w-xl shadow-inner shadow-slate-500 dark:shadow-slate-500 dark:border-b-[1px] border-slate-700 overflow-y-auto flex rounded-lg no-scrollbar">
+    <div className="items-center w-full h-56 mx-auto sm:mx-0 sm:max-w-xl shadow-inner shadow-slate-500 dark:shadow-slate-500 dark:border-b-[1px] border-slate-700 overflow-y-auto flex rounded-lg no-scrollbar justify-between flex-shrink-0">
       <OneStory />
-      
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
+      <OneStory />
     </div>
   );
 };
