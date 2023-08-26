28cc967871db17fb501d60b4a3e576054e6811d5
kailash8799 <kailashrajput8799@gmail.com>
2023-08-26 15:55:24 +0530

    added image

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import SideNavbar from "../subcomponents/SideNavbar";
+import ExplorePost from "./ExplorePost";
 
 const Explore = () => {
+    const exploreimages = [1,2,3,4,5,6,7,8,9];
   return (
-    <div className="min-h-screen bg-white dark:bg-black">
+    <div className="min-h-screen bg-white selection:bg-none dark:bg-black">
       <SideNavbar />
       <div className="md:px-20 sm:py-3 sm:px-5 lg:ml-64 sm:ml-16">
         <div className="w-full px-1 py-2 bg-white dark:bg-black sm:hidden">
@@ -13,18 +15,11 @@ const Explore = () => {
             className="w-full py-1.5  px-2 dark:text-white focus:outline-none text-black text-lg rounded-lg bg-neutral-500/30 dark:bg-neutral-600/50"
           />
         </div>
-        <div className="grid grid-cols-3 gap-2">
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
-          <div className="w-full h-full bg-red-500 aspect-square"></div>
+        <div className="grid grid-cols-3 gap-1 sm:gap-2">
+        {exploreimages?.map((image,ind)=>{
+            return <ExplorePost key={ind}/>
+        })}
+          
         </div>
       </div>
     </div>
